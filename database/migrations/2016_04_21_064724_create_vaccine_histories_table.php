978ff129aa9e13a6f32c8515fe94c63d99ad012c
jodypati <jodypati@gmail.com>
2016-04-23 15:56:00 +0700

    rename vaccineHistory to vaccine_histories

---
+++ database/migrations/2016_04_21_064724_create_vaccine_histories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVaccineHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vaccine_histories', function (Blueprint $table) {
+        $table->increments('id');
+        $table->integer('child_id')->unsigned();
+
+        $table->integer('vaccine_id')->unsigned();
+        $table->date('date');
+        $table->string('place');
+        $table->foreign('attribute_id')
+                  ->references('id')->on('attributes')
+                  ->onDelete('cascade');
+        $table->foreign('child_id')
+                  ->references('id')->on('children')
+                  ->onDelete('cascade');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('vaccineHistory');
+    }
+}
