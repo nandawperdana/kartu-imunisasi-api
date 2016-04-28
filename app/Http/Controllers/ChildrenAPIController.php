f7e0e6ef72d4fb39c8b95e7603efbd7bd37d267b
jodypati <jodypati@gmail.com>
2016-04-28 18:19:08 +0700

    create upload image function

--- app/Http/Controllers/ChildrenAPIController.php
+++ app/Http/Controllers/ChildrenAPIController.php
@@ -1,8 +1,12 @@
 <?php 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\Child;
 use App\User;
 use App\Acme\Transformers\ChildTransformer;
+use App\Http\Requests\ChildRequest;
+use Response;
 
 class ChildrenAPIController extends APIController
 {
@@ -15,22 +19,31 @@ class ChildrenAPIController extends APIController
 	
 	public function index($id = null)
 	{
-		$children = $id ? User::find($id) : Child::all();
-		if( ! $children ){
-			return $this->respondNotFound('children does not exists');
-		}else{
-			$children = $children->children;
-		}
+		if($id){
+			$children = User::find($id);
+			if( ! $children ){
+				return $this->respondNotFound('children does not exists');
+			}else{
+				$children = $children->children;
+			}
 
-		return $this->respond([
-			'data' => $this->childrenTransformer->transformCollection($children->all())
-		]);
+		}else{
+			$children = Child::all();
+		}
 		
+
+		return Response::json(
+			$this->childrenTransformer->transformCollection($children->all())
+		, 200);		
 	}
 
-	public function upload($id){
+	public function upload(Request $request,$id){
 		$FileNameFoto = null;
         $PathFoto = null;
+        $child = child::find($id);
+        if( ! $child ){
+			return $this->respondNotFound('Child does not exists');
+		}
         if($request->hasFile('FileNameFoto')){
             $FileNameFoto = time().'.'.$request->file('FileNameFoto')->getClientOriginalExtension();
             $PathFoto = '/images/children/' .$FileNameFoto;
@@ -39,25 +52,87 @@ class ChildrenAPIController extends APIController
             );
             $request['FileNameFoto'] = $FileNameFoto;
             $request['PathFoto'] = $PathFoto;
+            $child->update($request->all());
+	        return $this->respondCreated('Photo sucessfully uploaded.');
+        }else{
+        	return $this->respondCreated('Doesnt provide an image.');
         }
-        $child = child::findOrFail($id);
-        $child->update($request->all());
-        return $this->respondCreated('Photo sucessfully uploaded.');
+        
+        
+	}
+	public function upload(Request $request,$id){
+		$imgUsrFileName = null;
+		$imgUsrFilePath = null;
+        $user = user::find($id);
+        if( ! $user ){
+			return $this->respondNotFound('User does not exists');
+		}
+        if($request->hasFile('image')){
+            $imgUsrFileName = time().'.'.$request->file('image')->getClientOriginalExtension();
+            $imgUsrFilePath = '/images/parents/' .$imgUsrFileName;
+            $request->file('image')->move(
+                base_path() . '/public/images/parents/', $imgUsrFileName
+            );
+            $request['imgUsrFileName'] = $imgUsrFileName;
+            $request['imgUsrFilePath'] = $imgUsrFilePath;
+
+            
+        	$user->update($request->all());
+        	return $this->respondCreated('Photo sucessfully uploaded.');
+        }
+        
 	}
 
 	public function show($id, $id2 = null)
 	{
 		
-		$children = $id2 ? User::findOrFail($id)->child()->find($id2) : Child::find($id);
+		$children = $id2 ? User::findOrFail($id)->children->find($id2) : Child::find($id);
 	
 		if( ! $children ){
 			return $this->respondNotFound('children does not exists');
 		}
 		
 		return $this->respond([
-			'data'=>$this->childrenTransformer->transform($children)
+			$this->childrenTransformer->transform($children)
 		]);
 
 	}
+
+	/**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ChildRequest $request, $id)
+    {
+        $child = child::find($id);
+        
+        if( ! $child ){
+			return $this->respondNotFound('Child does not exists');
+		}else{
+        	$child->update($request->all());
+        	return $this->respondCreated('Child sucessfully updated.');
+    	}
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $child = child::find($id);
+        
+        if( ! $child ){
+			return $this->respondNotFound('Child does not exists');
+		}else{
+        	$child->delete();
+        	return $this->respondCreated('Child sucessfully deleted.');
+    	}
+    }
 
 }
--- app/Http/Controllers/UsersAPIController.php
+++ app/Http/Controllers/UsersAPIController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Http\Request;
-//use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Requests;
 //use App\Http\Controllers\Controller;
-//use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UploadRequest;
 use App\User;
 //use App\Book;
 use Response;
@@ -18,16 +19,18 @@ class UsersAPIController extends APIController
 	protected $userTransformer;
 	
 	function __construct(UserTransformer $userTransformer){
-		$this->middleware('jwt.auth',['only' => 'store']);		
+		$this->middleware('jwt.auth',['only' => ['store','update','destroy','upload','show']]);		
 		$this->userTransformer = $userTransformer;
+		//$this->middleware('jwt.refresh');
+
 	}
 	
 	public function index()
 	{
 		$users = User::all();
-		return Response::json([
-			'data' => $this->userTransformer->transformCollection($users->all())
-		], 200);
+		return Response::json(
+			$this->userTransformer->transformCollection($users->all())
+		, 200);
 		
 	}
 /*
@@ -35,14 +38,12 @@ class UsersAPIController extends APIController
     {
     }*/
 
-    public function store(){
+    public function store(UserRequest $request){
 		if( ! Input::get('name') or ! Input::get('gender')){
 			return $this->setStatusCode(422)
 						->respondWithError('Parameters failed validation for an User.');
 		}
-		
-		User::create(Input::all());
-		
+		$user = user::create($request->all());
 		return $this->respondCreated('User sucessfully created.');
 		/*return $this->setStatusCode(201)->respond([
 			'message' => 'User sucessfully created.',
@@ -58,7 +59,7 @@ class UsersAPIController extends APIController
 		}
 		
 		return $this->respond([
-			'data'=>$this->userTransformer->transform($user)
+			$this->userTransformer->transform($user)
 		]);
 		//return Response::json([
 			//'data' => $user->toArray()
@@ -67,35 +68,59 @@ class UsersAPIController extends APIController
 		//], 200);
 	}
 
-	public function upload($id){
+	public function upload(Request $request,$id){
 		$imgUsrFileName = null;
-        $imgUsrFilePath = null;
-        if($request->hasFile('imgUsrFileName')){
-            $imgUsrFileName = time().'.'.$request->file('imgUsrFileName')->getClientOriginalExtension();
+		$imgUsrFilePath = null;
+        $user = user::find($id);
+        if( ! $user ){
+			return $this->respondNotFound('User does not exists');
+		}
+        if($request->hasFile('image')){
+            $imgUsrFileName = time().'.'.$request->file('image')->getClientOriginalExtension();
             $imgUsrFilePath = '/images/parents/' .$imgUsrFileName;
-            $request->file('imgUsrFileName')->move(
+            $request->file('image')->move(
                 base_path() . '/public/images/parents/', $imgUsrFileName
             );
             $request['imgUsrFileName'] = $imgUsrFileName;
             $request['imgUsrFilePath'] = $imgUsrFilePath;
+
+            
+        	$user->update($request->all());
+        	return $this->respondCreated('Photo sucessfully uploaded.');
+        }else{
+        	return $this->respondCreated('Doesnt provide an image.');
         }
-        $user = user::findOrFail($id);
-        $user->update($request->all());
-        return $this->respondCreated('Photo sucessfully uploaded.');
+        
 	}
 /*
     public function edit($id)
     {
     }
+*/
 
     public function update(UserRequest $request, $id)
     {
+    	$user = user::find($id);
+        if( ! $user ){
+			return $this->respondNotFound('User does not exists');
+		}else{
+        	$user->update($request->all());
+        	return $this->respondCreated('User sucessfully updated.');
+    	}
     }
 
     public function destroy($id)
     {
-    }*/
-	/*
+    	$user = user::find($id);
+        if( ! $user ){
+			return $this->respondNotFound('User does not exists');
+		}else{
+    		$user = user::findOrFail($id);
+        	$user->delete();
+        	return $this->respondCreated('User sucessfully deleted.');
+    	}
+    }
+    /*
 	private function transformCollection($users){
 		return array_map([$this,'transform'], $users->toArray());
 	}
