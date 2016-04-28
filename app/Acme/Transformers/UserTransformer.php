2f46406f27e6cd2f1b267b712d1606d115da1577
jodypati <jodypati@gmail.com>
2016-04-28 18:15:19 +0700

    add id int JSON transformer

--- app/Acme/Transformers/UserTransformer.php
+++ app/Acme/Transformers/UserTransformer.php
@@ -5,23 +5,18 @@ class UserTransformer extends Transformer{
 	
 	public function transform($user){
 		return [
+			'id' => $user['id'],
 			'name' => $user['name'],
 			'email' => $user['email'],
 			'gender' => $user['gender'],
-			'avatarUrl' => $user['avatarUrl'],
 			'country' => $user['country'],
 			'state' => $user['state'],
 			'address' => $user['address'],
 			'phone' => $user['phone'],
-			'statusInfo' => $user['statusInfo'],
-			'profession' => $user['profession'],
 			'tempatLahir' => $user['tempatLahir'],
 			'tglLahir' => $user['tglLahir'],
-			'educLevId' => $user['educLevId'],
 			'imgUsrFileName' => $user['imgUsrFileName'],
-			'imgUsrFilePath' => $user['imgUsrFilePath'],
-			'lastStudy' => $user['lastStudy'],
-			'statusInfoUpAt' => $user['statusInfoUpAt']
+			'imgUsrFilePath' => $user['imgUsrFilePath']
 		];
 	}
 }
