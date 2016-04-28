9c7582ff634c0d5949540f89933f4945d785dda6
jodypati <jodypati@gmail.com>
2016-04-29 00:11:32 +0700

    fix class name issue

--- database/migrations/2016_04_21_064724_create_vaccine_histories_table.php
+++ database/migrations/2016_04_21_064724_create_vaccine_histories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVaccineHistoryTable extends Migration
+class CreateVaccineHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
