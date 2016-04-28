9d2a26c8931179c42f557dc5534aaa1f2c0ffc33
jodypati <jodypati@gmail.com>
2016-04-28 18:18:17 +0700

    fix problem showing children histories on index

--- app/Http/Controllers/VaccineHistoriesAPIController.php
+++ app/Http/Controllers/VaccineHistoriesAPIController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 use App\VaccineHistory;
 use App\Child;
 use App\Acme\Transformers\VaccineHistoryTransformer;
-
+use App\Http\Requests\VaccineHistoryRequest;
 class VaccineHistoriesAPIController extends APIController
 {
 	
@@ -15,17 +15,21 @@ class VaccineHistoriesAPIController extends APIController
 	
 	public function index($id = null)
 	{
-		$vaccineHistory = $id ? Child::find($id) : VaccineHistory::all();
-		
-		if( ! $vaccineHistory ){
-			return $this->respondNotFound('vaccineHistory does not exists');
+		if($id){
+			$vaccineHistory = Child::find($id);
+			if( ! $vaccineHistory ){
+				return $this->respondNotFound('History does not exists');
+			}else{
+				$vaccineHistory = $vaccineHistory->vaccineHistory;
+			}
+
 		}else{
-			$vaccineHistory = $vaccineHistory->vaccineHistory;
+			$vaccineHistory = VaccineHistory::all();
 		}
 
-		return $this->respond([
-			'data' => $this->vaccineHistoryTransformer->transformCollection($vaccineHistory->all())
-		]);
+		return $this->respond(
+			$this->vaccineHistoryTransformer->transformCollection($vaccineHistory->all())
+		);
 		
 	}
 
@@ -43,5 +47,42 @@ class VaccineHistoriesAPIController extends APIController
 		]);
 
 	}
+
+	/**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(VaccineHistoryRequest $request, $id)
+    {
+        $vaccineHistory = VaccineHistory::find($id);
+        
+        if( ! $vaccineHistory ){
+			return $this->respondNotFound('History does not exists');
+		}else{
+        	$vaccineHistory->update($request->all());
+        	return $this->respondCreated('History sucessfully updated.');
+    	}
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $vaccineHistory = VaccineHistory::find($id);
+        
+        if( ! $vaccineHistory ){
+			return $this->respondNotFound('History does not exists');
+		}else{
+        	$vaccineHistory->delete();
+        	return $this->respondCreated('History sucessfully deleted.');
+    	}
+    }
 
 }
